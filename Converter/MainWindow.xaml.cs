329125647b0f55dc116cf608873160d74ce95271
inarsadzba <inar.abhaz@gmail.com>
2018-09-12 18:40:28 +0300

    Загрузка на Git

--- Converter/MainWindow.xaml.cs
+++ Converter/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
-using Word = Microsoft.Office.Interop.Word;
+using Word = Microsoft.Office.Interop.Word; //
 
 namespace Converter
 {
